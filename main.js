4e03d3eb5d4b3cb94606181e3875015c5c0fb722
hour88 <hour88@Xubuntu.myguest.virtualbox.org>
2024-05-29 12:41:07 +0100

    book constructor and addBookToLibrary function added

---
+++ main.js
@@ -0,0 +1,16 @@
+const myLibrary = []
+
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+}
+
+function addBookToLibrary() {
+    let title = document.querySelector("#title").value;
+    let author = document.querySelector("#author").value;
+    let pages = document.querySelector("#pages").value;
+    let read = document.querySelector("#read").checked;
+    let newBook = new Book(title, author, pages, read);
+}
